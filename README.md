9652ef4b18c1d753302c1c6e2eec270b532f0f32
ChBlf <115346802+ChBlf@users.noreply.github.com>
2022-11-03 12:35:48 +0530

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# View color
-## by Blfr
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Main from './Components/Main';
-const root = createRoot(document.getElementById('root'))
-root.render(<Main />);
